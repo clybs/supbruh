15aef88d22183edb9197f075105fd77826f23c66
clybs_myrep <cliburn.solano@myrepublic.net>
2019-03-06 18:20:16 +0800

    Add cli dependencies

--- src/main/java/com/clybs/supbruh/App.java
+++ src/main/java/com/clybs/supbruh/App.java
@@ -1,13 +1,10 @@
 package com.clybs.supbruh;
 
 /**
- * Hello world!
- *
+ * Entry point
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
